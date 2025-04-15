ea62961aa743be030c793f40dc1bf454c30a02aa
Herberth Orlando Mancía Cisneros <herberthmeca@gmail.com>
2025-04-15 15:35:01 -0600

    Primeras_pruebas

---
+++ src/Varaibles/PracticaConstante.java
@@ -0,0 +1,11 @@
+package Varaibles;
+
+public class PracticaConstante {
+    public static void main(String[] args) {
+        int x = 10;
+        x =20;
+        x=30;
+        final double ANNUAL_INTEREST_RATE =0.05;
+    }
+
+}
---
+++ src/scanner/NumberCompare.java
@@ -0,0 +1,12 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class NumberCompare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int a = sc.nextInt();
+        System.out.println("Imprimir A:" + a);
+    }
+}
